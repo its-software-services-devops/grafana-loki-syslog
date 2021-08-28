e74a5de43fadc8a6ee47801449554951610af814
Seubpong Monsar <pjame.fb@gmail.com>
2021-08-28 22:02:34 +0700

    Initial commit

--- run-mixins.bash
+++ run-mixins.bash
@@ -3,6 +3,13 @@
 go get github.com/jsonnet-bundler/jsonnet-bundler/cmd/jb
 echo "$HOME/go/bin" >> "$GITHUB_PATH"
 
-jb
-jsonnet
-git
+WORKDIR=$(pwd)
+
+# === Loki Dashboards ===
+JSONNET_FILE=loki-dashboard.jsonnet
+git clone https://github.com/grafana/loki.git
+cd loki/production/loki-mixin
+cp ${WORKDIR}/${JSONNET_FILE} .
+jb install
+jsonnet -J vendor ${JSONNET_FILE} -m ${WORKDIR}/dashboard/generals
+cd ${WORKDIR}
