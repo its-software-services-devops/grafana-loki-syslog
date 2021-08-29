eaafffa7cd3a280bffd661f8c329e0b94c93b909
Seubpong Monsar <pjame.fb@gmail.com>
2021-08-29 10:30:11 +0700

    Use context path

--- run-mixins.bash
+++ run-mixins.bash
@@ -4,6 +4,7 @@ go get github.com/jsonnet-bundler/jsonnet-bundler/cmd/jb
 echo "$HOME/go/bin" >> "$GITHUB_PATH"
 
 WORKDIR=$(pwd)
+DASHBOARDs_DIR=${WORKDIR}/dashboards/generals
 
 # === Loki Dashboards ===
 JSONNET_FILE=loki-dashboards.jsonnet
@@ -23,12 +24,15 @@ jsonnet ${JSONNET_FILE} -m ${WORKDIR}/dashboards/generals
 # === Strimzi Kafka ===
 git clone https://github.com/strimzi/strimzi-kafka-operator.git
 cd strimzi-kafka-operator/examples/metrics/grafana-dashboards
-cp strimzi-kafka-exporter.json ${WORKDIR}/dashboards/generals
-cp strimzi-kafka.json ${WORKDIR}/dashboards/generals
-cp strimzi-operators.json ${WORKDIR}/dashboards/generals
-cp strimzi-zookeeper.json ${WORKDIR}/dashboards/generals
 
+SED_PATTERN='s#${DS_PROMETHEUS}#Prometheus-Platform#g'
 
+export FILE=strimzi-kafka-exporter.json; sed -i ${SED_PATTERN} ${FILE}; cp ${FILE} ${DASHBOARDs_DIR}
+export FILE=strimzi-kafka.json; sed -i ${SED_PATTERN} ${FILE}; cp ${FILE} ${DASHBOARDs_DIR}
+export FILE=strimzi-operators.json; sed -i ${SED_PATTERN} ${FILE}; cp ${FILE} ${DASHBOARDs_DIR}
+export FILE=strimzi-zookeeper.json; sed -i ${SED_PATTERN} ${FILE}; cp ${FILE} ${DASHBOARDs_DIR}
+
+sed -i "s#\$\{DS_PROMETHEUS\}#Prometheus-Platform#g" 
 # === Verification ===
 rm ${WORKDIR}/dashboards/generals/dummy.txt
 ls -lrt ${WORKDIR}/dashboards/generals
